798a2c757529312f85c260f39b34ec009f07a8f7
nickmaskaw <nickmaskaw@gmail.com>
2026-01-20 11:25:59 -0300

    Minor bug fix

--- src/teracontrol/hal/mercury_itc.py
+++ src/teracontrol/hal/mercury_itc.py
@@ -8,7 +8,7 @@ class MercuryITCController(GenericMercuryController):
     Hardware Abstraction layer (HAL) for a Mercury ITC controller.
     """
 
-    def __init__(timeout_s: float = 5.0):
+    def __init__(self, timeout_s: float = 5.0):
         super().__init__("Mercury ITC Controller", timeout_s)
 
     def get_status(self) -> dict[str, Any]:
