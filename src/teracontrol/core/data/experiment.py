fc70c768fb46b313061824519a64dcdae08730ac
MagnetoTHz <thzlab.ifusp@proton.me>
2026-01-26 17:29:53 -0300

    Runner test

--- src/teracontrol/core/data/experiment.py
+++ src/teracontrol/core/data/experiment.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass, field
+from datetime import datetime
 from typing import Any
 from .record import Record
 
 
 @dataclass
 class Experiment:
+    created_at: datetime = field(default_factory=datetime.now().astimezone().isoformat)
     metadata: dict[str, Any] = field(default_factory=dict)
     record: Record = field(default_factory=Record)
